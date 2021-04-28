d73d2e4272f1259675ef9b41bbdf7f1441b42aa8
Quando24 <stanley.raquan@gmail.com>
2021-04-27 20:23:18 -0400

    finalized readme

---
+++ Assets/README.md
@@ -0,0 +1,28 @@
+
+  # 🏁 Special-ReadMe-Generator  🏁
+  # Description ✏️
+  ### It is a readme generator, that serves the purpose of providing a template, and allows the user to enter answers into the questions generated, to create a readme quickly! 
+  # Table of Contents 🗃️
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contributors)
+  * [Credits](#credits)
+  * [License](#license)
+  
+  # Installation 📖
+  In the terminal, an important step, is to do the following: input npm init -y, hit enter, and then input npmi inquirer, and hit enter. This is necessay in order to make Node.js function as intended in the terminal. 
+  ## Usage 📜
+  To develop a readme quickly for all contributions pushed to GitHub. 
+  ## Contribution 🤝
+  Fellow classmates - Chris Lyons, Amy Smerlick, and Stephen Dever 
+  ### Instructions 📝
+  run in the terminal
+  ## Credits 📋
+  emojis - https://github.com/ikatyang/emoji-cheat-sheet/blob/master/README.md#light--video 
+  ## License
+  The application you're enjoying is covered under the Apache license.
+  [![License: Apache](https://img.shields.io/badge/License-Apache-yellow.svg)](https://www.apache.org/licenses/LICENSE-2.0.txt)
+  # Questions 💡
+    For questions, please email me, or contact me via GitHub.
+  * Email✉️: raquan.stanley@gmail.com
+  * GitHub🖥️: Quando24 
--- Assets/index.js
+++ Assets/index.js
@@ -1,28 +1,19 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer'); 
+const generatorMarkdown = require("./utils/generateMarkdown")
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-const inquirer = require('inquirer');
-const fs = require('fs');
 inquirer.prompt([
   {
     type: 'input',
-    message: 'Please enter your project title?',
+    message: 'What is your project title?',
     name: 'title',
     validate: (value) => { if (value) { return true } else { return "Please enter a value to continue" } },
   },
   {
     type: 'input',
-    message: 'Please enter your project description?',
+    message: 'Please enter your project description',
     name: 'description',
     validate: (value) => { if (value) { return true } else { return "Please enter a value to continue" } },
   },
@@ -30,95 +21,105 @@ inquirer.prompt([
     type: 'input',
     message: 'Please describe the Installation',
     name: 'installation',
-    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue"  } },
+    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue" } },
   },
   {
     type: 'input',
     message: 'Please describe the usage',
     name: 'usage',
-    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue"  } },
+    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue" } },
   },
   {
     type: 'input',
     message: 'Please list your Contributors',
     name: 'contributors',
-    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue"  } },
+    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue" } },
   },
   {
     type: 'input',
     message: 'Please list your Instructions',
     name: 'instructions',
-    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue"  } },
+    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue" } },
   },
   {
     type: 'input',
     message: 'Please list your credits',
     name: 'credits',
-    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue"  } },
+    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue" } },
   },
   {
     type: 'list',
     message: 'Please choose your license',
     name: 'license',
     choices: ['GNU', 'Apache', 'Boost'],
-    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue"  } },
+    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue" } },
   },
   {
     type: 'input',
     message: 'Please enter your GitHub path',
     name: 'gitHub',
-    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue"  } },
+    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue" } },
   },
   {
     type: 'input',
     message: 'Please enter your email address',
     name: 'email',
-    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue"  } },
+    validate: (value) => { if (value) { return true } else { return "Please enter a value to continue" } },
   }
 ]
 )
-.then((data) => {
-  let licenseLink = ""
-  if (data.license==="Apache") {
-   licenseLink =  "https://www.apache.org/licenses/LICENSE-2.0.txt"
-  }
-  else if (data.license==="GNU"){
-    licenseLink = "https://choosealicense.com/licenses/lgpl-3.0/#"
-  }
-  else {
-    licenseLink="Boost"
-  }
-  const filename = 'README.md';
-  const readMe = `
-  # ${data.title}
-  # Description
+  .then((data) => {
+    let licenseLink = ""
+    if (data.license === "Apache") {
+      licenseLink = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+    }
+    else if (data.license === "GNU") {
+      licenseLink = "https://choosealicense.com/licenses/lgpl-3.0/#"
+    }
+    else {
+      licenseLink = "Boost"
+    }
+    const filename = 'README.md';
+    const readMe = `
+  # 🏁 ${data.title} 🏁
+  # Description ✏️
   ### ${data.description}
-  # Table of Contents
+  # Table of Contents 🗃️
   * [Installation](#installation)
   * [Usage](#usage)
   * [Contribution](#contributors)
   * [Credits](#credits)
   * [License](#license)
   
-  # Installation
+  # Installation 📖
   ${data.installation}
-  ## Usage
+  ## Usage 📜
   ${data.usage}
-  ## Contribution
+  ## Contribution 🤝
   ${data.contributors}
-  ### Instructions
+  ### Instructions 📝
   ${data.instructions}
-  ## Credits
+  ## Credits 📋
   ${data.credits}
   ## License
   The application you're enjoying is covered under the ${data.license} license.
   [![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-yellow.svg)](${licenseLink})
-  # Questions
+  # Questions 💡
     For questions, please email me, or contact me via GitHub.
-  * Email: ${data.email}
-  * GitHub: ${data.gitHub}`;
-  console.log(readMe);
-  fs.writeFile(filename, readMe, (err) =>
-    err ? console.log(err) : console.log('Success!')
-  );
-});
+  * Email ✉️: ${data.email}
+  * GitHub 🖥️: ${data.gitHub}`;
+    console.log(readMe);
+    fs.writeFile(filename, readMe, (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
+
+
